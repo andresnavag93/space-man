392b7aa646524423da8a7b5c79870b3478a3060c
Andres Navarro <andresnavag93@gmail.com>
2021-11-17 15:32:57 -0400

    :hammer: Player jump configuration

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,16 +6,50 @@ public class PlayerController : MonoBehaviour
 {
     public int jumpForce = 6;
     Rigidbody2D playerRigidBody;
-    
+    public LayerMask groundMask;
+
+    [SerializeField]
+    float raycastLong = 1.5f;
+
+    void Awake()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+
+        Debug.DrawRay(this.transform.position, Vector2.down * raycastLong, Color.green);
+    }
+
+    void Jump()
+    {
+        if (isTouchingTheGround())
+        {
+            playerRigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    bool isTouchingTheGround()
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, raycastLong, groundMask))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
