32ed96da87f1d439226dd289118a6bdcc6ed33f1
Andres Navarro <andresnavag93@gmail.com>
2021-11-18 17:53:40 -0400

    :hammer: Level Manager Configuration

---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager sharedInstance;
+
+    public List<LevelBlock> allTheLevelBlocks = new List<LevelBlock>();
+    public List<LevelBlock> currentLevelBlocks = new List<LevelBlock>();
+    public Transform levelStartPosition;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddLevelBlock(){
+
+    }
+
+    public void RemoveLevelBlock(){
+        
+    }
+
+    public void RemoveAllLevelBlocks(){
+        
+    }
+
+    public void GenerateInitialBlocks(){
+        for (int i = 0; i < 2; i++)
+        {
+            AddLevelBlock();
+        }
+    }
+}
