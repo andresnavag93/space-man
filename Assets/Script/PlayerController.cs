4b7c1c2ff12d01f4db68d7652dad589277c2382d
Andres Navarro <andresnavag93@gmail.com>
2021-11-17 21:44:39 -0400

    :hammer: Player movements

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int jumpForce = 6;
-    Rigidbody2D playerRigidBody;
+    public float jumpForce = 6f;
+    public float runningSpeed = 2f;
+
+    Rigidbody2D rigidBody;
     public LayerMask groundMask;
 
     [SerializeField]
     float raycastLong = 1.5f;
+    Animator animator;
+
+    const string STATE_ALIVE = "isAlive";
+    const string STATE_ON_THE_GROUND = "isOnTheGround";
+    const string STATE_RUNNING = "isRunning";
 
     void Awake()
     {
-        playerRigidBody = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_THE_GROUND, true);
+        animator.SetBool(STATE_RUNNING, false);
     }
 
     // Update is called once per frame
@@ -30,14 +40,49 @@ public class PlayerController : MonoBehaviour
             Jump();
         }
 
+        animator.SetBool(STATE_ON_THE_GROUND, isTouchingTheGround());
         Debug.DrawRay(this.transform.position, Vector2.down * raycastLong, Color.green);
+    }
+
+    void FixedUpdate()
+    {
+        move();
+        //moveWithKey();
+    }
+
+    void move() {
+        
+        if ( rigidBody.velocity.x < runningSpeed) {
+            animator.SetBool(STATE_RUNNING, true);
+            rigidBody.velocity = new Vector2( runningSpeed, rigidBody.velocity.y );
+        }
+    }
+    
+    void moveWithKey() {
+
+        if (rigidBody.velocity.x == 0 && isTouchingTheGround()) {
+            animator.SetBool(STATE_RUNNING, false);
+        }
+
+        rigidBody.velocity = new Vector2 ( Input.GetAxis("Horizontal")*runningSpeed, rigidBody.velocity.y );
+
+        if (Input.GetAxis("Horizontal") < 0){
+            animator.SetBool(STATE_RUNNING, true);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        else if (Input.GetAxis("Horizontal") > 0){
+            animator.SetBool(STATE_RUNNING, true);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
     }
 
     void Jump()
     {
         if (isTouchingTheGround())
         {
-            playerRigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
@@ -45,10 +90,12 @@ public class PlayerController : MonoBehaviour
     {
         if (Physics2D.Raycast(this.transform.position, Vector2.down, raycastLong, groundMask))
         {
+            // animator.enabled = true;
             return true;
         }
         else
         {
+            // animator.enabled = false;
             return false;
         }
     }
